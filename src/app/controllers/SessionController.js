533f956380700df4c9adaed418a5d76de2aaafae
Diego Souza <dee-souza@hotmail.com>
2019-08-05 11:39:36 -0300

    Feat: Update user and password

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -6,21 +6,15 @@ class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
 
-    const user = await User.findOne({
-      where: { email },
-    });
+    const user = await User.findOne({ where: { email } });
 
     if (!user) {
       // Not Authorized 401
-      return res.status(401).json({
-        error: 'User not found.',
-      });
+      return res.status(401).json({ error: 'User not found.' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({
-        error: 'Password does not match.',
-      });
+      return res.status(401).json({ error: 'Password does not match.' });
     }
 
     const { id, name } = user;
