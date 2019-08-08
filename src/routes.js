a32e324a9d3f1b05213f9745c86366ba5bf3fe00
Diego Souza <dee-souza@hotmail.com>
2019-08-08 16:57:49 -0300

    Feat: List appointments to a provider user

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,6 +23,9 @@ routes.put('/users', UserController.update);
 
 // Provider
 routes.get('/providers', ProviderController.index);
+
+// Schedule
+routes.get('/schedules', ScheduleController.index);
 
 // Appointments
 routes.get('/appointments', AppointmentController.index);
