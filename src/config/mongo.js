e8e3c4f60bc55828cf9c3c4c8b5dc88c95ae3cb6
Diego Souza <dee-souza@hotmail.com>
2019-08-09 10:09:10 -0300

    Feat: Install mongodb docker

---
+++ src/config/mongo.js
@@ -0,0 +1,4 @@
+export default {
+  url: 'mongodb://192.168.99.100:27017/',
+  database: 'gobarber',
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,11 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
 import databaseConfig from '../config/database';
+import mongoConfig from '../config/mongo';
 
 const models = [User, File, Appointment];
 
@@ -17,6 +20,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      `${mongoConfig.url}/${mongoConfig.database}`,
+      { useNewUrlParser: true, useFindAndModidy: true }
+    );
   }
 }
 
