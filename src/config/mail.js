3d04d26ae594831d260bc3316b9a0067eeff2d00
Diego Souza <dee-souza@hotmail.com>
2019-08-09 11:36:36 -0300

    Feat: Add nodemailer and config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  post: 2525,
+  secure: false,
+  auth: {
+    user: 'd4c7991ae22312',
+    pass: '49ba24e62e8dee',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
